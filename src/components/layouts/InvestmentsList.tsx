be6bf1f74ee7c4ce49ad0cf21cf779dc581ac13e
Christine Kim <chrismekim@gmail.com>
2020-11-30 12:01:46 -0600

    in progress

--- src/components/layouts/InvestmentsList.tsx
+++ src/components/layouts/InvestmentsList.tsx
@@ -4,8 +4,10 @@ import withBreakpoints, {
   MediaQuery,
   InjectedProps as WithBreakpointsProps,
 } from 'lib/withBreakpoints';
-import { InvestmentsListItem } from 'lib/cms/types';
-import { Button } from 'components/base';
+import { InvestmentsListItem, Founder, Company } from 'lib/cms/types';
+import generateFounderFullName from 'utils/generateFounderFullName';
+
+import { Img, Button } from 'components/base';
 import { LineIcon } from 'components/icons';
 import PortableText from 'components/PortableText';
 
@@ -19,10 +21,11 @@ type PassedProps = {
 
 type Props = PassedProps & WithBreakpointsProps;
 const InvestmentsList: React.FC<Props> = ({ heading, items, className, mediaQuery }) => {
+  console.log(heading);
   return (
     <div className={cx(`InvestmentsList col-12 flex flex-row flex-wrap`, className)}>
-      <div className="InvestmentsList__heading uppercase color-charcoal primary-sm pb2_25 md:pb3_75">
-        {/* {heading} */}
+      <div className="InvestmentsList__heading uppercase color-charcoal primary-sm pb_75 md:pb2_25">
+        {heading}
       </div>
 
       {items.map((item: InvestmentsListItem) => (
@@ -42,14 +45,27 @@ const InvestmentListItem = (item: InvestmentsListItem, mediaQuery: MediaQuery) =
 
   return (
     <div className="InvestmentsList__item-inner-container col-12 flex flex-row py1_5">
-      <div className="secondary-sm col-3 pr1 md:pr0">{item.company.name}</div>
-      <div className="InvestmentsList__heading-container primary-md col-8">
-        <span className="">
+      <div className="secondary-sm col-3 pr1 md:pr0 flex flex-col">
+        <div className="InvestmentList__founders uppercase">
+          {item.company.founders?.map((founder: Founder) => (
+            <div>{generateFounderFullName(founder)} </div>
+          ))}
+        </div>
+        <div className="primary-sm color-lilac uppercase">{item.company.name}</div>
+        {item.company.tag && <div className="md:pt1_5">{item.company.tag}</div>}
+      </div>
+
+      <div className="InvestmentsList__logo-container col-3">
+        <Img src={item.company.logo} className="w100 h100" />
+      </div>
+
+      <div className="InvestmentsList__description primary-md col-6">
+        <span className="secondary-sm">
           <PortableText blocks={item.description.paragraph} />{' '}
         </span>
 
         {item.description.button?.link && (
-          <span className="InvestmentsList__button">
+          <span className="InvestmentsList__button secondary-sm">
             <Button
               wrap={true}
               className="inline-flex items-center text-decoration-none secondary-sm-bold vertical-align-middle color-charcoal"
--- src/views/companies.tsx
+++ src/views/companies.tsx
@@ -26,7 +26,9 @@ const CompaniesView: FC<Props> = (props) => {
         introByline={hero.introByline}
         briefParagraph={hero.briefParagraph}
       />
-      <InvestmentsList heading={investmentsList.heading} items={investmentsList.items} />
+      <div className="text-module-padding-x border-bottom-lilac pt3_75 lg:pt10">
+        <InvestmentsList heading={investmentsList.heading} items={investmentsList.items} />
+      </div>
       <TextModule
         className="text-module-padding-x py3_75 lg:py10"
         variant="default"
