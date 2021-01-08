38540de3266f6080af8673b7ca7974276f3678e3
Christine Kim <chrismekim@gmail.com>
2021-01-08 16:55:43 -0600

    Add PageNotFound styling

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch, bindActionCreators } from 'redux';
-import cx from 'classnames';
+import Language from 'constants/Language';
 
 import { setTheme } from 'state/actions/applicationActions';
 
 import Meta from 'components/Meta';
-import TextModuleWithParagraphs from 'components/layouts/TextModuleWithParagraphs';
+import LineIconWithButton from 'components/icons/LineIconWithButton';
 
 import { Theme } from 'types';
-
-// import Language from 'constants/Language';
 
 interface DispatchProps {
   actions: {
@@ -31,23 +29,26 @@ class PageNotFound extends Component<Props> {
     return (
       <>
         <Meta />
-        <div className="bg-color-mulberry">
-          <div className="TextModuleWithParagraphs--inner-container mxauto col-12 lg:col-8 xxl:col-7 px_75lg:px1_5 vh100 color-white flex flex-col justify-center">
-            <span
-              className={cx(
-                `TextModuleWithParagraphs__subheading primary-sm pr3_75 vertical-align-middle inline`
-              )}
-            >
-              Error 404
-            </span>
+        <div className="bg-color-mulberry site-padding-x">
+          <div className="mxauto col-12 lg:col-8 xxl:col-7 px_75lg:px1_5 vh100 color-white flex flex-col justify-center">
+            <div className="">
+              <span className="primary-sm pr3_75 vertical-align-middle inline">
+                {Language.t('PageNotFound.error404')}
+              </span>
 
-            <span
-              className={cx(
-                `TextModuleWithParagraphs__heading TextModuleWithParagraphs--style-$__heading primary-xl vertical-align-middle`
-              )}
-            >
-              We weren't able to find the page you were looking for.
-            </span>
+              <span className="primary-xl vertical-align-middle">
+                {Language.t('PageNotFound.heading')}
+              </span>
+
+              <p className="secondary-sm color-white md:col-8 pt1_5 md:pt2_25">
+                {Language.t('PageNotFound.description')}
+              </p>
+
+              <p className="secondary-sm color-white md:col-8 pt1_5 md:pt2_25">
+                {Language.t('PageNotFound.solutions')}
+                <LineIconWithButton color="white" link="/" label="Return to the home page" />
+              </p>
+            </div>
           </div>
         </div>
       </>
