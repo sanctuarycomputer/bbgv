ef7b76eedb57f07b9106ccecb8bf439763e667be
Christine Kim <chrismekim@gmail.com>
2021-01-12 15:20:48 -0600

    In progress

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+// import React from 'react';
+// import { withRouter } from 'react-router-dom';
 
-class ScrollToTop extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
+// class ScrollToTop extends React.Component {
+//   componentDidUpdate(prevProps) {
+//     if (this.props.location.pathname !== prevProps.location.pathname) {
+//       window.scrollTo(0, 0);
+//     }
+//   }
+
+//   render() {
+//     return null;
+//   }
+// }
+
+// export default withRouter(ScrollToTop);
+
+import { useEffect, useRouter } from 'react';
+
+const ScrollToTop = () => {
+  const { pathname, search } = useRouter();
+
+  useEffect(() => {
+    try {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    } catch (error) {
       window.scrollTo(0, 0);
     }
-  }
+  }, [pathname, search]);
 
-  render() {
-    return null;
-  }
-}
+  return null;
+};
 
-export default withRouter(ScrollToTop);
+export default ScrollToTop;
