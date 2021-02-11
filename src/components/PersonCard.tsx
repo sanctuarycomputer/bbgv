cad218b7d4da3f0eeb0a6b7f80f3088928a11529
Christine Kim <chrismekim@gmail.com>
2021-02-11 16:44:50 -0600

    revert bio changes

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -26,14 +26,14 @@ const PersonCard: React.FC<Props> = ({
   bio,
 }) => {
   return (
-    <div className="PersonCard flex flex-row">
+    <div className="PersonCard">
       <Img
         src={image.src}
         alt={image.alt || Language.t('Global.fallbackAltLabel')}
-        className="PersonCard__image fit-cover radius-xs w100 h100 mr_75 mb1_25 md:mr1_2 col-6"
+        className="inline PersonCard__image fit-cover radius-xs w100 h100 mr_75 mb1_25 md:mr1_25 md:mb1_5"
       />
 
-      <span className="col-6">
+      <span>
         <div className="uppercase primary-sm flex flex-col pb1_5 md:pb3_75">
           <p className="color-charcoal">{fullName}</p>
           <p className="color-lilac-very-dark">{jobTitle}</p>
@@ -85,7 +85,7 @@ const PersonCard: React.FC<Props> = ({
         </div>
 
         {bio && (
-          <span className="secondary-xs">
+          <span className="secondary-sm">
             <PortableText blocks={bio} />
           </span>
         )}
